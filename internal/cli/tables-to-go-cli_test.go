57707d6e04c58ef143966527177f1ed62d4415c6
Frank Meyer <fr4nk235@gmail.com>
2019-04-12 21:14:55 +0800

    Add first PoC unit-test for the Run() function

--- internal/cli/tables-to-go-cli_test.go
+++ internal/cli/tables-to-go-cli_test.go
@@ -4,7 +4,64 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/fraenky8/tables-to-go/pkg/config"
+	"github.com/fraenky8/tables-to-go/pkg/database"
+	"github.com/fraenky8/tables-to-go/pkg/output"
 )
+
+type mockMySQLDb struct {
+	mock.Mock
+
+	*database.MySQL
+
+	tables []*database.Table
+}
+
+func newMockMySQLDb(settings *config.Settings) *mockMySQLDb {
+	return &mockMySQLDb{
+		MySQL: database.NewMySQL(settings),
+	}
+}
+
+func (db *mockMySQLDb) Connect() (err error) {
+	db.Called()
+	return nil
+}
+
+func (db *mockMySQLDb) Close() (err error) {
+	db.Called()
+	return nil
+}
+
+func (db *mockMySQLDb) GetTables() (tables []*database.Table, err error) {
+	db.Called()
+	return db.tables, nil
+}
+
+func (db *mockMySQLDb) PrepareGetColumnsOfTableStmt() (err error) {
+	db.Called()
+	return nil
+}
+
+func (db *mockMySQLDb) GetColumnsOfTable(table *database.Table) (err error) {
+	db.Called(table)
+	return nil
+}
+
+type mockWriter struct {
+	mock.Mock
+}
+
+func newMockWriter() *mockWriter {
+	return &mockWriter{}
+}
+
+func (w *mockWriter) Write(tableName string, content string) error {
+	w.Called(tableName, content)
+	return nil
+}
 
 func Test_toInitialisms(t *testing.T) {
 	tests := []struct {
@@ -57,6 +114,72 @@ func Test_toInitialisms(t *testing.T) {
 		t.Run(tt.desc, func(t *testing.T) {
 			actual := toInitialisms(tt.intput)
 			assert.Equal(t, tt.expected, actual, "test case input: "+tt.intput)
+		})
+	}
+}
+
+func TestRun(t *testing.T) {
+	tests := []struct {
+		desc     string
+		settings func() *config.Settings
+		db       func(*config.Settings) database.Database
+		writer   func() output.Writer
+		isError  assert.ErrorAssertionFunc
+	}{
+		{
+			desc: "mysql: single table with integer column",
+			settings: func() *config.Settings {
+				s := config.NewSettings()
+				s.DbType = config.DbTypeMySQL.String()
+				return s
+			},
+			db: func(settings *config.Settings) database.Database {
+				db := newMockMySQLDb(settings)
+
+				table := &database.Table{
+					Name: "test_table",
+					Columns: []database.Column{
+						{
+							OrdinalPosition: 1,
+							Name:            "user_id",
+							DataType:        "integer",
+						},
+					},
+				}
+				db.tables = append(db.tables, table)
+
+				db.
+					On("GetTables").
+					Return(db.tables, nil)
+				db.
+					On("PrepareGetColumnsOfTableStmt").
+					Return(nil)
+				db.
+					On("GetColumnsOfTable", table)
+
+				return db
+			},
+			writer: func() output.Writer {
+				w := newMockWriter()
+				w.
+					On(
+						"Write",
+						"TestTable",
+						"package dto\n\ntype TestTable struct {\nUserID sql.NullString `db:\"user_id\"`\n}",
+					)
+				return w
+			},
+			isError: assert.NoError,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			s := test.settings()
+			db := test.db(s)
+			w := test.writer()
+
+			err := Run(s, db, w)
+			test.isError(t, err)
 		})
 	}
 }
