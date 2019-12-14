4e7ec5209a2fe23aaab29040a0924d7d5786f6e6
Frank Meyer <fr4nk235@gmail.com>
2019-12-14 15:52:15 +0800

    add first POC for integration tests via dockertest package
    - Used MySQL 8 as first candidate
    - Added build flags
    - Added test data for temporal data types
    - Added and vendor'ed new dependencies

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -22,6 +22,7 @@ var (
 
 // Database interface for the concrete databases.
 type Database interface {
+	SQLDriver() *sqlx.DB
 	DSN() string
 	Connect() error
 	Close() error
@@ -78,9 +79,10 @@ type Column struct {
 // databases it implements partly the Database interface.
 type GeneralDatabase struct {
 	GetColumnsOfTableStmt *sqlx.Stmt
-	*sqlx.DB
+	driver                string
+
 	*settings.Settings
-	driver string
+	*sqlx.DB
 }
 
 // New creates a new Database based on the given type in the settings.
@@ -118,6 +120,11 @@ func (gdb *GeneralDatabase) Connect(dsn string) (err error) {
 	}
 
 	return gdb.Ping()
+}
+
+// SQLDriver returns the underlyig SQL driver
+func (gdb *GeneralDatabase) SQLDriver() *sqlx.DB {
+	return gdb.DB
 }
 
 // Close closes the database connection.
