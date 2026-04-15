17a5140ec7628a9ff1b06e57422701ec92873863
Frank Meyer <fr4nk235@gmail.com>
2026-04-15 15:19:42 +0800

    main: refactor
    Heavily inspired by https://grafana.com/blog/how-i-write-http-services-in-go-after-13-years/#func-main-only-calls-run
    
    - Add run function and pass needed arguments.
    - Use Stderr to print the messages (help, version, ...)
    - Close the DB in any case.
    - Make printVersion more accurate.

--- tables-to-go.go
+++ tables-to-go.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"os/signal"
 	"runtime"
 	"runtime/debug"
 
@@ -19,99 +22,126 @@ var (
 	buildTimestamp = ""
 )
 
-// CmdArgs represents the supported command line args
-type CmdArgs struct {
+// cmdArgs represents the supported command line args
+type cmdArgs struct {
 	Help    bool
 	Version bool
+	usage   func()
 	*settings.Settings
 }
 
-// NewCmdArgs creates and prepares the command line arguments with default values
-func NewCmdArgs() (args *CmdArgs) {
+// newCmdArgs creates and prepares the command line arguments with default values
+func newCmdArgs(args []string) *cmdArgs {
+	fs := flag.NewFlagSet(args[0], flag.ExitOnError)
 
-	args = &CmdArgs{
+	a := cmdArgs{
+		usage:    fs.Usage,
 		Settings: settings.New(),
 	}
 
-	flag.BoolVar(&args.Help, "?", false, "shows help and usage")
-	flag.BoolVar(&args.Help, "help", false, "shows help and usage")
-	flag.BoolVar(&args.Verbose, "v", args.Verbose, "verbose output")
-	flag.BoolVar(&args.VVerbose, "vv", args.VVerbose, "more verbose output")
-	flag.BoolVar(&args.Version, "version", args.Version, "show version and build information")
-	flag.BoolVar(&args.Force, "f", args.Force, "force; skip tables that encounter errors")
+	fs.BoolVar(&a.Help, "?", false, "shows help and usage")
+	fs.BoolVar(&a.Help, "help", false, "shows help and usage")
+	fs.BoolVar(&a.Verbose, "v", a.Verbose, "verbose output")
+	fs.BoolVar(&a.VVerbose, "vv", a.VVerbose, "more verbose output")
+	fs.BoolVar(&a.Version, "version", a.Version, "show version and build information")
+	fs.BoolVar(&a.Force, "f", a.Force, "force; skip tables that encounter errors")
 
-	flag.Var(&args.DbType, "t", fmt.Sprintf("type of database to use, currently supported: %v", settings.SprintfSupportedDbTypes()))
-	flag.StringVar(&args.User, "u", args.User, "user to connect to the database")
-	flag.StringVar(&args.Pswd, "p", args.Pswd, "password of user")
-	flag.StringVar(&args.DbName, "d", args.DbName, "database name; for sqlite3, URL query params '_pragma=<fn()>' can be added, e.g. ?_pragma=busy_timeout(5000)")
-	flag.StringVar(&args.Schema, "s", args.Schema, "schema name")
-	flag.StringVar(&args.Host, "h", args.Host, "host of database")
-	flag.StringVar(&args.Port, "port", args.Port, "port of database host, if not specified, it will be the default ports for the supported databases")
-	flag.StringVar(&args.SSLMode, "sslmode", args.SSLMode, "Connect to database using secure connection. (default \"disable\")\nThe value will be passed as is to the underlying driver.\nRefer to this site for supported values: https://www.postgresql.org/docs/current/libpq-ssl.html")
-	flag.StringVar(&args.Socket, "socket", args.Socket, "The socket file to use for connection. If specified, takes precedence over host:port.")
-	flag.Var(&args.Tables, "table", "Filter for the specified table(s). Can be used multiple times or with comma separated values without spaces. Example: -table foobar -table foo,bar,baz")
+	fs.Var(&a.DbType, "t", fmt.Sprintf("type of database to use, currently supported: %v", settings.SprintfSupportedDbTypes()))
+	fs.StringVar(&a.User, "u", a.User, "user to connect to the database")
+	fs.StringVar(&a.Pswd, "p", a.Pswd, "password of user")
+	fs.StringVar(&a.DbName, "d", a.DbName, "database name; for sqlite3, URL query params '_pragma=<fn()>' can be added, e.g. ?_pragma=busy_timeout(5000)")
+	fs.StringVar(&a.Schema, "s", a.Schema, "schema name")
+	fs.StringVar(&a.Host, "h", a.Host, "host of database")
+	fs.StringVar(&a.Port, "port", a.Port, "port of database host, if not specified, it will be the default ports for the supported databases")
+	fs.StringVar(&a.SSLMode, "sslmode", a.SSLMode, "Connect to database using secure connection. (default \"disable\")\nThe value will be passed as is to the underlying driver.\nRefer to this site for supported values: https://www.postgresql.org/docs/current/libpq-ssl.html")
+	fs.StringVar(&a.Socket, "socket", a.Socket, "The socket file to use for connection. If specified, takes precedence over host:port.")
+	fs.Var(&a.Tables, "table", "Filter for the specified table(s). Can be used multiple times or with comma separated values without spaces. Example: -table foobar -table foo,bar,baz")
 
-	flag.StringVar(&args.OutputFilePath, "of", args.OutputFilePath, "output file path, default is current working directory")
-	flag.Var(&args.OutputFormat, "format", "format of struct fields (columns): camelCase (c) or original (o)")
+	fs.StringVar(&a.OutputFilePath, "of", a.OutputFilePath, "output file path, default is current working directory")
+	fs.Var(&a.OutputFormat, "format", "format of struct fields (columns): camelCase (c) or original (o)")
 
-	flag.Var(&args.FileNameFormat, "fn-format", "format of the filename: camelCase (c, default) or snake_case (s)")
-	flag.StringVar(&args.Prefix, "pre", args.Prefix, "prefix for file- and struct names")
-	flag.StringVar(&args.Suffix, "suf", args.Suffix, "suffix for file- and struct names")
-	flag.StringVar(&args.PackageName, "pn", args.PackageName, "package name")
-	flag.Var(&args.Null, "null", "representation of NULL columns: sql.Null* (sql) or primitive pointers (native|primitive)")
+	fs.Var(&a.FileNameFormat, "fn-format", "format of the filename: camelCase (c, default) or snake_case (s)")
+	fs.StringVar(&a.Prefix, "pre", a.Prefix, "prefix for file- and struct names")
+	fs.StringVar(&a.Suffix, "suf", a.Suffix, "suffix for file- and struct names")
+	fs.StringVar(&a.PackageName, "pn", a.PackageName, "package name")
+	fs.Var(&a.Null, "null", "representation of NULL columns: sql.Null* (sql) or primitive pointers (native|primitive)")
 
-	flag.BoolVar(&args.NoInitialism, "no-initialism", args.NoInitialism, "disable the conversion to upper-case words in column names")
+	fs.BoolVar(&a.NoInitialism, "no-initialism", a.NoInitialism, "disable the conversion to upper-case words in column names")
 
-	flag.BoolVar(&args.TagsNoDb, "tags-no-db", args.TagsNoDb, "do not create db-tags")
+	fs.BoolVar(&a.TagsNoDb, "tags-no-db", a.TagsNoDb, "do not create db-tags")
 
-	flag.BoolVar(&args.TagsMastermindStructable, "tags-structable", args.TagsMastermindStructable, "generate struct with tags for use in Masterminds/structable (https://github.com/Masterminds/structable)")
-	flag.BoolVar(&args.TagsMastermindStructableOnly, "tags-structable-only", args.TagsMastermindStructableOnly, "generate struct with tags ONLY for use in Masterminds/structable (https://github.com/Masterminds/structable)")
-	flag.BoolVar(&args.IsMastermindStructableRecorder, "structable-recorder", args.IsMastermindStructableRecorder, "generate a structable.Recorder field")
+	fs.BoolVar(&a.TagsMastermindStructable, "tags-structable", a.TagsMastermindStructable, "generate struct with tags for use in Masterminds/structable (https://github.com/Masterminds/structable)")
+	fs.BoolVar(&a.TagsMastermindStructableOnly, "tags-structable-only", a.TagsMastermindStructableOnly, "generate struct with tags ONLY for use in Masterminds/structable (https://github.com/Masterminds/structable)")
+	fs.BoolVar(&a.IsMastermindStructableRecorder, "structable-recorder", a.IsMastermindStructableRecorder, "generate a structable.Recorder field")
 
-	// disable the print of usage when an error occurs
-	flag.CommandLine.Usage = func() {}
+	// NOOP to disable the print of usage when an error occurs.
+	fs.Usage = func() {}
 
-	flag.Parse()
+	// Ignore error since we are using flag.ExitOnError
+	_ = fs.Parse(args[1:])
 
-	return args
+	return &a
 }
 
 // main function to run the transformations
 func main() {
+	ctx := context.Background()
+	if err := run(ctx, os.Args, os.Stderr); err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
 
-	cmdArgs := NewCmdArgs()
+func run(ctx context.Context, args []string, stderr io.Writer) (err error) {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	_ = ctx // TODO propagate down
+
+	cmdArgs := newCmdArgs(args)
 
 	if cmdArgs.Help {
-		flag.Usage()
-		os.Exit(0)
+		cmdArgs.usage()
+		return nil
 	}
 
 	if cmdArgs.Version {
-		printVersion()
-		os.Exit(0)
+		printVersion(stderr)
+		return nil
 	}
 
 	if err := cmdArgs.Verify(); err != nil {
-		fmt.Print(err)
-		os.Exit(1)
+		return err
 	}
 
 	db := database.New(cmdArgs.Settings)
+	defer func(db database.Database) {
+		if cErr := db.Close(); cErr != nil {
+			if err != nil {
+				err = fmt.Errorf("could not close database: %w, previous error: %w", cErr, err)
+				return
+			}
+			err = fmt.Errorf("could not close database: %w", cErr)
+		}
+	}(db)
 
 	if err := db.Connect(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		return fmt.Errorf("could not connect to database: %w", err)
 	}
 
 	writer := output.NewFileWriter(cmdArgs.OutputFilePath)
 
 	if err := cli.Run(cmdArgs.Settings, db, writer); err != nil {
-		fmt.Printf("run error: %v\n", err)
-		os.Exit(1)
+		return fmt.Errorf("run error: %w", err)
 	}
+
+	return nil
 }
 
-func printVersion() {
+func printVersion(stderr io.Writer) {
+	var (
+		goOS, goArch = runtime.GOOS, runtime.GOARCH
+	)
 	info, ok := debug.ReadBuildInfo()
 	if ok {
 		if versionTag == "" {
@@ -121,19 +151,20 @@ func printVersion() {
 			switch s.Key {
 			case "vcs.revision":
 				revision = s.Value[:8]
-			}
-			if s.Key == "vcs.revision" {
-				revision = s.Value[:8]
+			case "GOOS":
+				goOS = s.Value
+			case "GOARCH":
+				goArch = s.Value
 			}
 		}
 	}
 
-	fmt.Printf("tables-to-go/%s-%s %s/%s built with %s", versionTag, revision,
-		runtime.GOOS, runtime.GOARCH, runtime.Version())
+	_, _ = fmt.Fprintf(stderr, "tables-to-go/%s-%s %s/%s built with %s",
+		versionTag, revision, goOS, goArch, runtime.Version())
 
 	//goland:noinspection GoBoolExpressions
 	if buildTimestamp != "" {
-		fmt.Printf(" on %s", buildTimestamp)
+		_, _ = fmt.Fprintf(stderr, " on %s", buildTimestamp)
 	}
-	fmt.Println()
+	_, _ = fmt.Fprintln(stderr)
 }
